295352097b79af93b91fcc418e6dde854910c643
Dharun Kumar S H <freebirdprod.official@gmail.com>
2026-05-14 11:07:57 +0530

    Github Connect

--- components/custom/WorkspaceBody.tsx
+++ components/custom/WorkspaceBody.tsx
@@ -1,13 +1,31 @@
 "use client";
 import { UserDetailContext } from "@/context/UserDetailContext";
 import Image from "next/image";
-import React, { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { Card, CardContent } from "../ui/card";
 import EmptyWorkspace from "./EmptyWorkspace";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
-const WorkspaceBody = () => {
+function WorkspaceBody() {
   const { userDetail } = useContext(UserDetailContext);
+  const router = useRouter();
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    GetGithubUserToken();
+  }, []);
+
+  const GetGithubUserToken = async () => {
+    const result = await axios.get("/api/github/token");
+    console.log(result.data.token);
+    setToken(result.data.token);
+  };
+
+  const onAddRepo = async () => {
+    router.push("/api/github");
+  };
   return (
     <div>
       <div className="flex items-center justify-between">
@@ -22,7 +40,11 @@ const WorkspaceBody = () => {
           <h2 className="text-lg">Connect GitHub & Add Repo</h2>
         </div>
         <div>
-          <Button> Install</Button>
+          {!token ? (
+            <Button onClick={onAddRepo}>Setup</Button>
+          ) : (
+            <Button>+ Add Repo</Button>
+          )}
         </div>
       </Card>
       <Card className="mt-10">
@@ -32,6 +54,6 @@ const WorkspaceBody = () => {
       </Card>
     </div>
   );
-};
+}
 
 export default WorkspaceBody;
